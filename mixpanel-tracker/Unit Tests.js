4e811c269739511ca2c65104c2f8408c26d6e922
Yuhui <yuhuibc@gmail.com>
2019-07-10 11:01:37 +0800

    docs: add Copyright notices

--- mixpanel-tracker/Unit Tests.js
+++ mixpanel-tracker/Unit Tests.js
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2019 Yuhui. All Rights Reserved.
+ *
+ * Licensed under the GNU General Public License, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file Contains unit test methods for the library.
+ */
+
 var TEST_DISTINCT_ID_ = 'abc12345';
 var TEST_TOKEN_ = 'get-this-from-project-settings';
 var TEST_PAYLOAD_ = {
